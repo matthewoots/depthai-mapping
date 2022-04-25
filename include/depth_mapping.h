2dcda2518373bb7d09374b74fc11157241c5699b
matthewoots <matthewoots@gmail.com>
2022-04-25 18:59:16 +0800

    Depth to mapping initial commit, conversion is not working

--- include/depth_mapping.h
+++ include/depth_mapping.h
@@ -1,6 +1,10 @@
 #include <string>
 #include <iostream>
 #include <memory>
+#include <queue>
+
+#include <Eigen/Dense>
+#include <math.h>
 
 #include "ros/ros.h"
 
@@ -10,11 +14,39 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
 
 
 namespace depthai_ros
 {
     using CV_mat_ptr = std::shared_ptr<cv::Mat>;
+
+    class depth_mapping_node
+    {
+        public:
+
+            double depth_lower_limit, depth_upper_limit;
+            Eigen::Matrix3f intrinsics_;
+
+            void init(double ll, double ul, double sub_fact);
+
+            void calc_pcl_pointcloud(cv::Mat depthmap);
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr local_pc;
+
+        private:
+
+            Eigen::Matrix3Xf inverse_project_depthmap_into_3d(cv::Mat depthmap);
+
+            Eigen::MatrixXf convert_depthmap_to_eigen_row_matrix(cv::Mat depthmap);
+
+            double subsample_factor;
+            
+    };
 
     /**
      * DepthMapPublisher is a ROS node which launches OAK-D streams as specified in the config file.
@@ -24,6 +56,25 @@ namespace depthai_ros
     class depth_publisher_node
     {
         public:
+
+            std::mutex depth_mutex;
+            std::queue<std::pair<float, cv::Mat>> depth_queue;
+
+            /**
+             * @param intrinsics (const Eigen::Matrix3f&) : 3x3 camera intrinsics
+             *
+             * camera intrinsics (K)
+             * |f_u   0   o_u|
+             * | 0   f_v  0_v|
+             * | 0    0    1 |
+             *
+             * (f_u, f_v) -> focal length (horizontal, vertical)
+             * (o_u, o_v) -> principal point (horizontal, vertical)
+             */
+            Eigen::Matrix3f intrinsics;
+
+            depth_mapping_node map_node;
+
             /**
              * Constructor
              */
@@ -42,30 +93,60 @@ namespace depthai_ros
 
             void update_depth_timer(const ros::TimerEvent &);
 
+            void update_map(const ros::TimerEvent &);
+
             bool device_poll();
+
+            void clear_depth_queue();
+
+            sensor_msgs::PointCloud2 pcl2ros_converter(pcl::PointCloud<pcl::PointXYZ>::Ptr _pc);
+
+            void update_depth_pair(cv::Mat depth_mat)
+            {
+                std::lock_guard<std::mutex> d_lock(depth_mutex);
+                depth_w_stamp.first = ros::Time::now().toSec();
+                depth_w_stamp.second = depth_mat;
+                return;
+            }
+
+            cv::Mat get_depth_pair()
+            {
+                std::lock_guard<std::mutex> d_lock(depth_mutex);
+                cv::Mat new_mat = depth_w_stamp.second;
+                return new_mat;
+            }
+
+            bool check_depth_mat()
+            {
+                if(depth_w_stamp.second.empty())
+                    return false;
+                else
+                    return true;
+            }
 
         private:
             ros::NodeHandle _nh;
 
-            ros::Timer update_depth;
-            ros::Publisher depth_image_pub, disparity_image_pub;
+            ros::Timer update_depth, map_timer;
+            ros::Publisher depth_image_pub, disparity_image_pub, local_pcl_pub;
 
             // Class Private Variables
             std::string _mono_resolution;
             std::string _depth_map_topic;
 
             int _confidence, _lr_check_thres, _median_kernal_size;
+            int _subsample; 
 
             bool _lr_check, _extended, _subpixel;
             bool init = false, registered = false;
-            
+        
             double _update_interval;
-            double _max_range_clamp;
+            double _max_range_clamp, _min_range_clamp;
 
             double empty_poll_count_time = 0.0;
-            double image_width_in_pixels, focal_length_in_pixels;
+            double image_width_in_pixels, image_height_in_pixels, focal_length_in_pixels;
             double baseline;
-            double hfov;
+            double hfov, vfov;
 
             ros::Time node_start_time;
 
@@ -81,10 +162,7 @@ namespace depthai_ros
             dai::Pipeline dev_pipeline;
             std::shared_ptr<dai::Device> device;
             std::shared_ptr<dai::DataOutputQueue> disparity_data;
-    };
 
-    class depth_mapping_node
-    {
-        
+            std::pair<double, cv::Mat> depth_w_stamp;
     };
 }
--- src/depth_mapping.cpp
+++ src/depth_mapping.cpp
@@ -4,10 +4,22 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+
 #include "depth_mapping.h"
 
 namespace depthai_ros
 {
+
+    /**
+     * @brief For depth_publisher_node class
+     */
 
     depth_publisher_node::depth_publisher_node(
         ros::NodeHandle &nodeHandle) : _nh(nodeHandle)
@@ -38,10 +50,14 @@ namespace depthai_ros
         _nh.param<bool>("extended", _extended, true);
         _nh.param<bool>("subpixel", _subpixel, false);
 
+        _nh.param<int>("subsample", _subsample, 1);
+
         depth_image_pub = _nh.advertise<sensor_msgs::Image>(
-            _depth_map_topic, 20);
+            _depth_map_topic, 3);
         disparity_image_pub = _nh.advertise<sensor_msgs::Image>(
-            "/disparity", 20);
+            "/disparity", 3);
+        local_pcl_pub = _nh.advertise<sensor_msgs::PointCloud2>(
+            "/pcl", 3);
 
         setup_depth_stream();
 
@@ -49,6 +65,10 @@ namespace depthai_ros
 
         update_depth = _nh.createTimer(ros::Duration(_update_interval), 
             &depth_publisher_node::update_depth_timer, this, false, false);
+
+        map_timer = _nh.createTimer(ros::Duration(_update_interval), 
+            &depth_publisher_node::update_map, this, false, false);
+
         update_depth.start();
     }
 
@@ -82,6 +102,13 @@ namespace depthai_ros
             device = std::make_shared<dai::Device>(dev_pipeline);
             // Get output queue
             disparity_data = device->getOutputQueue("disparity", 4, false);
+            
+            // Start with init mapping node
+            map_node.init(_min_range_clamp, _max_range_clamp, _subsample);
+            map_node.intrinsics_ = intrinsics;
+
+            map_timer.start();
+            
             init = true;
         }
         
@@ -111,6 +138,11 @@ namespace depthai_ros
         depth_img = depth_img / 100.0;
         depth_img = min(depth_img, _max_range_clamp);
 
+        update_depth_pair(depth_img);
+
+        // Add to the queue
+        // depth_queue.push(depth_w_stamp);
+
         // double min_dist, max_dist;
         // minMaxLoc(depth_img, &min_dist, &max_dist); // Find minimum and maximum intensities
         // ROS_INFO("Min %lfm Max %lfm\n", min_dist, max_dist);
@@ -124,6 +156,21 @@ namespace depthai_ros
         bridge_msg.encoding = sensor_msgs::image_encodings::TYPE_32FC1;
 
         depth_image_pub.publish(bridge_msg.toImageMsg());
+    }
+
+    void depth_publisher_node::update_map(const ros::TimerEvent &)
+    {
+        if (!check_depth_mat())
+            return;
+
+        map_node.calc_pcl_pointcloud(get_depth_pair());
+
+        sensor_msgs::PointCloud2 local_pcl_msg;
+        local_pcl_msg = pcl2ros_converter(map_node.local_pc);
+
+        local_pcl_msg.header.stamp = ros::Time::now();
+        local_pcl_msg.header.frame_id = "/base_link";
+        local_pcl_pub.publish(local_pcl_msg); 
     }
 
     bool depth_publisher_node::device_poll()
@@ -141,6 +188,25 @@ namespace depthai_ros
         return false;
     }
 
+    void depth_publisher_node::clear_depth_queue()
+    {
+        std::lock_guard<std::mutex> d_lock(depth_mutex);
+        while (!depth_queue.empty()) 
+            depth_queue.pop();
+    }
+
+    /** 
+    * @brief Convert point cloud from ROS sensor message to 
+    * pcl point ptr
+    */
+    sensor_msgs::PointCloud2
+        depth_publisher_node::pcl2ros_converter(pcl::PointCloud<pcl::PointXYZ>::Ptr _pc)
+    {
+        sensor_msgs::PointCloud2 ros_msg;
+        pcl::toROSMsg(*_pc, ros_msg);
+        return ros_msg;
+    }
+
 
     void depth_publisher_node::setup_depth_stream()
     {
@@ -149,30 +215,34 @@ namespace depthai_ros
 
         dai::node::MonoCamera::Properties::SensorResolution mono_res;
         baseline = 7.5;
-        hfov = 71.9;
+        hfov = 72.0; vfov = 50.0;
 
         // 1280 x 720
         if(_mono_resolution == "720p")
         {
             image_width_in_pixels = 1280.0;
+            image_height_in_pixels = 720.0;
             mono_res = dai::node::MonoCamera::Properties::SensorResolution::THE_720_P; 
         }
         // 640 x 400
         else if(_mono_resolution == "400p" )
         {
             image_width_in_pixels = 640.0;
+            image_height_in_pixels = 400.0;
             mono_res = dai::node::MonoCamera::Properties::SensorResolution::THE_400_P; 
         }
         // 1280 x 800
         else if(_mono_resolution == "800p" )
         {
             image_width_in_pixels = 1280.0;
+            image_height_in_pixels = 800.0;
             mono_res = dai::node::MonoCamera::Properties::SensorResolution::THE_800_P; 
         }
         // 640 x 480
         else if(_mono_resolution == "480p" )
         {
             image_width_in_pixels = 640.0;
+            image_height_in_pixels = 480.0;
             mono_res = dai::node::MonoCamera::Properties::SensorResolution::THE_480_P; 
         }
         else{
@@ -231,12 +301,135 @@ namespace depthai_ros
         mono_right->out.link(stereo_depth->right);
         stereo_depth->disparity.link(xout->input);
 
+        _min_range_clamp = focal_length_in_pixels * baseline / stereo_depth->initialConfig.getMaxDisparity();
+
         ROS_INFO("Horizontal resolution %lfpix", image_width_in_pixels);
         ROS_INFO("Max disparity %lfpix", stereo_depth->initialConfig.getMaxDisparity());
-        ROS_INFO("Focal length %lfpix", focal_length_in_pixels);
+        ROS_INFO("Focal length horizontal %lfpix", focal_length_in_pixels);
         ROS_INFO("Baseline %lfcm, HFOV %lfdeg", baseline, hfov);
+        ROS_INFO("Min stereo distance %lfm", _min_range_clamp/100.0);
+
+        // Set up intrinsics
+        /**
+         * camera intrinsics (K)
+            * |f_u   0   o_u|
+            * | 0   f_v  0_v|
+            * | 0    0    1 |
+         * 
+         */
+        auto focal_length_v_in_pixels = image_height_in_pixels * 0.5 / tan(vfov * 0.5 * M_PI / 180.0); 
+
+        
+        intrinsics << focal_length_in_pixels, 0, image_width_in_pixels,
+                    0, focal_length_v_in_pixels, image_height_in_pixels,
+                    0, 0, 1;
     }
 
+    
 
+
+    /**
+     * @brief For depth_mapping_node class
+     */
+
+    void depth_mapping_node::init(double ll, double ul, double sub_fact)
+    {
+        depth_lower_limit = ll;
+        depth_upper_limit = ul;
+        subsample_factor = sub_fact;
+    }
+
+    /**
+     * @brief get_pcl_pointcloud
+     * From https://github.com/InverseProject/pose-landmark-graph-slam
+     */
+    void depth_mapping_node::calc_pcl_pointcloud(cv::Mat depthmap)
+    {
+        ros::Time start_pcl = ros::Time::now();
+        Eigen::Matrix3Xf points_3d = inverse_project_depthmap_into_3d(depthmap);
+
+        ROS_INFO("1. Inverse_project_depthmap_into_3d succeeded");
+        int valid_points_cnt = 0;
+        ROS_INFO("points_3d Size %d", points_3d.cols());
+        int total_points = points_3d.cols();
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr empty(new pcl::PointCloud<pcl::PointXYZ>);
+        local_pc = empty;
+
+        // reserve memory space for optimization
+        local_pc->reserve(total_points);
+
+        for (int i = 0; i < total_points; i += subsample_factor)
+        {
+            // if (points_3d(2, i) >= depth_lower_limit && points_3d(2, i) < depth_upper_limit)
+            // {
+                local_pc->push_back(pcl::PointXYZ(points_3d(0, i), points_3d(1, i), points_3d(2, i)));
+            //     valid_points_cnt++;
+            // }
+        }
+
+        // local_pc->width = valid_points_cnt;
+        // local_pc->height = 1;
+        // local_pc->is_dense = true;
+        // local_pc->resize(valid_points_cnt);
+        ROS_INFO("Pointcloud Size %d", local_pc->points.size());
+        ROS_INFO("5. Pointcloud completed %lf\n", (ros::Time::now() - start_pcl).toSec());
+    }
+
+    /**
+     * @brief inverse_project_depthmap_into_3d
+     * From https://github.com/InverseProject/pose-landmark-graph-slam
+     */
+    Eigen::Matrix3Xf depth_mapping_node::inverse_project_depthmap_into_3d(cv::Mat depthmap)
+    {
+        // depthmap's size of column (width), row (height), and total number of pixels
+        int depthmap_col = depthmap.cols;
+        int depthmap_row = depthmap.rows;
+        ROS_INFO("cols() %d rows() %d", depthmap_col, depthmap_row);
+        int total_pixels = depthmap_col * depthmap_row;
+
+        // creating column index vector : resulting in a row vector (1,total_pixels)
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> col_idx_flat_row_vec =
+            Eigen::RowVectorXf::LinSpaced(depthmap_col, 0, depthmap_col - 1).replicate(depthmap_row, 1);
+        col_idx_flat_row_vec.resize(1, total_pixels);
+
+        ROS_INFO("2. Created col_idx_flat_row_vec");
+
+        // creating row index vector : resulting in a row vector (1,total_pixels)
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> row_idx_flat_row_vec =
+            Eigen::VectorXf::LinSpaced(depthmap_row, 0, depthmap_row - 1).replicate(1, depthmap_col);
+        row_idx_flat_row_vec.resize(1, total_pixels);
+
+        ROS_INFO("3. Created row_idx_flat_row_vec");
+
+        // creating row matrix filled with ones : resulting in a row vector (1,total_pixels)
+        auto one_flat_row_vec = Eigen::MatrixXf::Ones(1, total_pixels);
+
+        // getting depth value inside a 2D depth map as a row vector (1,total_pixels)
+        Eigen::MatrixXf depth_flat_row_vec = convert_depthmap_to_eigen_row_matrix(depthmap);
+        ROS_INFO("4. convert_depthmap_to_eigen_row_matrix succeeded");
+
+        Eigen::Matrix3Xf points(3, total_pixels);
+        points.row(0) = col_idx_flat_row_vec;
+        points.row(1) = row_idx_flat_row_vec;
+        points.row(2) = one_flat_row_vec;
+
+        return intrinsics_.inverse() * points * depth_flat_row_vec.asDiagonal();
+    }
+
+    /**
+     * @brief convert_depthmap_to_eigen_row_matrix
+     * From https://github.com/InverseProject/pose-landmark-graph-slam
+     */
+    Eigen::MatrixXf depth_mapping_node::convert_depthmap_to_eigen_row_matrix(cv::Mat depthmap)
+    {
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> mtrx;
+        cv::cv2eigen(depthmap, mtrx);
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> row_mtrx = mtrx;
+
+        row_mtrx.resize(1, depthmap.rows * depthmap.cols);
+
+        return row_mtrx;
+    }
 
 }
